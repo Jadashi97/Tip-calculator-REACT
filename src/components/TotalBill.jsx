fee8b0a4f51f7f51214faf1521c8af02ddae8b71
jadashi97 <njadauniversal23@yahoo.com>
2023-04-07 16:36:38 -0500

    (feat): added functionality. (fix): make sure app is fully functioning

--- src/components/TotalBill.jsx
+++ src/components/TotalBill.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 const TotalBill = ()=> {
     
     const [bill, setBill] = useState('');
-    const [tip, setTip] = useState("10%");
+    const [tipPercentage, setTipPercentage] = useState("10%");
+    const [tipAmount, setTipAmount] = useState(0);
+    const [splitTotal, setSplitTotal] = useState("$0.00");
+    const [split, setSplit] = useState(1);
 
+    function handleBillChange(e){
+        let value =  e.target.value;
+        setBill(value);
+    }
+
+    function handleTipChange(e){
+        let value = e.target.value.replace('%','');
+        if(value.indexOf('%') === -1){
+            value = value+ '%';
+        }
+        setTipPercentage(value);
+    }   
+
+
+    function increasePeople(){
+        setSplit(oldValue => oldValue + 1)
+    }
+
+    function decreasePeople(){
+        // returns 1 if the resulting value is less than 1 & ensures that the value of split is always at least 1.
+        setSplit(oldValue => Math.max(oldValue - 1, 1)); 
+    }
+
+    function calculate(){
+        const percentage = 1 + parseInt(tipPercentage.replace('%',''))/100;
+        const result = (bill * percentage / split).toFixed(2);
+        setSplit(result);
+    }
+
+    useEffect(()=>{
+        calculate()
+    }, [bill,tipPercentage, split])
 
     return (
         <div>
@@ -18,19 +53,19 @@ const TotalBill = ()=> {
                         placeholder={"0.00"} 
                         id='tipInput'
                         value={bill}
-                        onChange={e => setBill(e.target.value)}
+                        onChange={handleBillChange}
                     />
                 </div>
             </div>
             <div className="container">
             <div className="title">Tip</div>
                 <div className="inputContainer">
-                    <span>%</span>
                     <input 
                         type="text" 
                         id="tipInput" 
                         placeholder={"10"} 
-                        onChange={e => setTip(e.target.value)}
+                        value={tipPercentage}
+                        onChange={handleTipChange}
                     />
                 </div>
             </div>
@@ -39,13 +74,13 @@ const TotalBill = ()=> {
                     <div className="title">People</div>
                     <div className="controls">
                         <span className="buttonContainer">
-                            <button className="splitButton" onClick={"increasePeople()"}>
+                            <button className="splitButton" onClick={increasePeople}>
                                 <span className="buttonText">+</span>
                             </button>
                         </span>
-                        <span className="splitAmount" id="numberOfPeople">1</span>
+                        <span className="splitAmount" id="numberOfPeople">{split}</span>
                         <span className="buttonContainer">
-                            <button className="splitButton" onClick={"decreasePeople()"}>
+                            <button className="splitButton" onClick={decreasePeople}>
                             <span className="buttonText">-</span>
                             </button>
                         </span>
@@ -53,7 +88,7 @@ const TotalBill = ()=> {
                 </div>
                 <div className="totalContainer">
                     <div className="title">Total per Person</div>
-                    <div className="total" id="perPersonTotal">$0.00</div>
+                    <div className="total" id="perPersonTotal">{splitTotal}</div>
                 </div>
             </div>
         </div>
