e383393690c3374e406656cbfdd114c21a65e487
jadashi97 <njadauniversal23@yahoo.com>
2023-04-06 20:42:11 -0500

    (feat): figure out the inital state and useState for the app. (fix): add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import './App.css';
 import Header from './components/Header';
-import MainCalcPage from './components/SplitCalculateBill';
 import TotalBill from './components/TotalBill';
-import SplitCalculateBill from './components/SplitCalculateBill';
 
 function App() {
 
@@ -12,7 +10,6 @@ function App() {
       <Header/>
       <div className="wrapper">
         <TotalBill/>
-        <SplitCalculateBill/>
       </div>
     </div>
   )
--- src/components/TotalBill.jsx
+++ src/components/TotalBill.jsx
@@ -1,34 +1,62 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-const TotalBill = () => {
-  return (
-    <div>
-        <div className="container" id="topContainer">
-            <div className="title">Bill total</div>
-            <div className="inputContainer">
-                <span>$</span>
-                <input 
-                    onKeyUp="calculateBill()" 
-                    type="text" 
-                    id="billTotalInput" 
-                    placeholder="0.00" 
-                />
+const TotalBill = ()=> {
+    
+    const [bill, setBill] = useState('');
+    const [tip, setTip] = useState("10%");
+
+
+    return (
+        <div>
+            <div className="container" id="topContainer">
+                <div className="title">Bill total</div>
+                <div className="inputContainer">
+                    <span>$</span>
+                    <input 
+                        type="text" 
+                        placeholder={"0.00"} 
+                        id='tipInput'
+                        value={bill}
+                        onChange={e => setBill(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="container">
+            <div className="title">Tip</div>
+                <div className="inputContainer">
+                    <span>%</span>
+                    <input 
+                        type="text" 
+                        id="tipInput" 
+                        placeholder={"10"} 
+                        onChange={e => setTip(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="container" id="bottom">
+                <div className="splitContainer">
+                    <div className="title">People</div>
+                    <div className="controls">
+                        <span className="buttonContainer">
+                            <button className="splitButton" onClick={"increasePeople()"}>
+                                <span className="buttonText">+</span>
+                            </button>
+                        </span>
+                        <span className="splitAmount" id="numberOfPeople">1</span>
+                        <span className="buttonContainer">
+                            <button className="splitButton" onClick={"decreasePeople()"}>
+                            <span className="buttonText">-</span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
+                <div className="totalContainer">
+                    <div className="title">Total per Person</div>
+                    <div className="total" id="perPersonTotal">$0.00</div>
+                </div>
             </div>
         </div>
-        <div className="container">
-        <div className="title">Tip</div>
-            <div className="inputContainer">
-                <span>%</span>
-                <input 
-                    onKeyUp="calculateBill()" 
-                    type="text" 
-                    id="tipInput" 
-                    placeholder="10" 
-                />
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
-
 export default TotalBill;
